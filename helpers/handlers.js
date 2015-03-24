ca5e17a6c2e056cb393ca12edc9d8f8705083d64
Feyzo <feyzullahdaldal@gmail.com>
2015-03-24 14:13:27 +0200

    Refactoring for reuse

--- helpers/handlers.js
+++
@@ -1,4 +0,0 @@
-{
-    "twilio" : "function() { var instance = scxml.createInstance(); scxml.send(instance, {name: 't'}); res.send('twilio, ' + instance.getConfiguration()); }",
-    "twitter": "function() { res.send('twitter' + req.path + scxml); }"
-}
