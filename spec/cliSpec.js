3ad42c20addca44de8a311ac71c9cc9ab81ef086
Feyzo <feyzullahdaldal@gmail.com>
2015-03-30 22:06:42 +0300

    Refactored to use util for tests

--- spec/cliSpec.js
+++ spec/cliSpec.js
@@ -3,95 +3,35 @@
 /* global describe, beforeEach, afterEach, it, expect */
 
 var nixt = require('nixt'),
-  fs = require('fs'),
-  path = require('path'),
-  yaml = require('js-yaml'),
-  port = 6002,
-  smaasJSON = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname + '/./../smaas.yml'), 'utf8'));
-
-smaasJSON.host = 'localhost:' + port;
-
-var tempPath = path.resolve(__dirname + '/.tmp/'),
-  baseApi = '/api/v1/',
-  client = 'node ' + path.resolve(__dirname + '/../client.js') + ' -H http://localhost:' + port + ' ';
-
-var passToTestRunner, server;
+  util = require('./util')();
 
 describe('SCXML-CLI', function () {
   beforeEach(function(done) {
-    //Cleanup and prepare temp folder
-    nixt()
-      .run('rm -rf ' + tempPath)
-      .run('mkdir ' + tempPath)
-      .exist(tempPath)
-      .end(done);
-
-    var app = require('express')();
-
-    //Serve swagger client API
-    app.get('/smaas.json', function (req, res) {
-      res.status(200).send(smaasJSON);
-    });
-
-    app.all('*', function (req, res) {
-      //Pass request to specified interceptor or fail
-      if(passToTestRunner) passToTestRunner(req, res);
-      else res.send(404);
-    });
-
-    //Start listening for API calls
-    server = app.listen(port);
+    util.beforeEach(done);
   });
 
   afterEach(function (done) {
-    //Cleanup temp folder
-    nixt()
-      .run('rm -rf ' + tempPath)
-      .end(done);
-
-    // Remove API interceptor
-    passToTestRunner = null;
-
-    //Close the server
-    server.close();
+    util.afterEach(done);
   });
 
-  function checkStderr (result) {
-    expect(result.stderr.length).toBe(0);
-  }
-
-  function createHelloWorld (done) {
-    // In order:
-    // Change directory to temp
-    // Run create command
-    // Check for stderr
-    // Check file existance
-    nixt()
-      .run(client + 'create ' + tempPath + '/helloworld.scxml')
-      .expect(checkStderr)
-      .exist(tempPath + '/helloworld.scxml')
-      .end(done);
-  }
-
   it('should create helloworld file locally', function (done) {
-    createHelloWorld(done);
+    util.createHelloWorld(done);
   });
 
   it('should save helloworld.scxml with various options', function (done) {
-    passToTestRunner = function (req, res) {
+    util.passToTestRunner = function (req, res) {
       // Intercepted API call should be: PUT /api/v1/helloworld.scxml with application/xml
-      expect(req.path).toBe(baseApi + 'helloworld.scxml');
+      expect(req.path).toBe(util.baseApi + 'helloworld.scxml');
       expect(req.method).toBe('PUT');
       expect(req.headers['content-type']).toBe('application/xml');
       res.sendStatus(201);
     };
 
-    // Create helloworld
-    createHelloWorld(function () {
+    util.createHelloWorld(function () {
       //Save created file
       nixt()
-        .run(client + 'save ' + tempPath + '/helloworld.scxml')
-        .expect(checkStderr)
+        .run(util.client + 'save ' + util.tempPath + '/helloworld.scxml')
+        .expect(util.checkStderr)
         .end(done);
     });
   });
---
+++ spec/util.js
@@ -0,0 +1,78 @@
+'use strict';
+// jshint node: true
+/* global expect */
+
+var nixt = require('nixt'),
+  fs = require('fs'),
+  path = require('path'),
+  yaml = require('js-yaml');
+
+module.exports = function(opts) {
+  opts = opts || {};
+  opts.port = opts.port || 6002;
+  opts.smaasJSON = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname + '/./../smaas.yml'), 'utf8'));
+  opts.smaasJSON.host = 'localhost:' + opts.port;
+  opts.tempPath = path.resolve(__dirname + '/.tmp/');
+  opts.baseApi = '/api/v1/';
+  opts.client = 'node ' + path.resolve(__dirname + '/../client.js') + ' -H http://localhost:' + opts.port + ' ';
+  
+  opts.beforeEach = function (done) {
+
+    var app = require('express')();
+
+    //Serve swagger client API
+    app.get('/smaas.json', function (req, res) {
+      res.status(200).send(opts.smaasJSON);
+    });
+
+    app.all('*', function (req, res) {
+      //Pass request to specified interceptor or fail
+      if(opts.passToTestRunner) opts.passToTestRunner(req, res);
+      else res.sendStatus(404);
+    });
+
+    //Start listening for API calls
+    opts.server  = app.listen(opts.port);
+
+    nixt()
+      .run('rm -rf ' + opts.tempPath)
+      .run('mkdir ' + opts.tempPath)
+      .exist(opts.tempPath)
+      .end(done);
+  };
+
+  opts.afterEach = function (done) {
+    //Cleanup temp folder
+    nixt()
+      .run('rm -rf ' + opts.tempPath)
+      .end(done);
+
+    // Remove API interceptor
+    opts.passToTestRunner = null;
+
+    //Close the server
+    opts.server.close();
+  };
+
+  opts.checkStderr = function (result) {
+    expect(result.stderr.length).toBe(0);
+    if(result.stderr.length > 0) {
+      console.log(result.stderr);
+    }
+  };
+
+  opts.createHelloWorld = function (done) {
+    // In order:
+    // Change directory to temp
+    // Run create command
+    // Check for stderr
+    // Check file existance
+    nixt()
+      .run(opts.client + 'create ' + opts.tempPath + '/helloworld.scxml')
+      .expect(opts.checkStderr)
+      .exist(opts.tempPath + '/helloworld.scxml')
+      .end(done);
+  };
+
+  return opts;
+};
