6fda1189b8aa81ae29deb28afc57cdfeb03a3eaa
Feyzo <feyzullahdaldal@gmail.com>
2015-04-01 13:03:39 +0300

    Added send tests

--- spec/sendSpec.js
+++ spec/sendSpec.js
@@ -3,6 +3,7 @@
 /* global describe, beforeEach, afterEach, it, expect */
 
 var nixt = require('nixt'),
+  fs = require('fs'),
   util = require('./util')();
 
 var instanceId = 'helloworld.scxml/helloinstance',
@@ -37,6 +38,7 @@ describe('SCXML-CLI - send', function () {
       .end(done);
   });
 
+  // TODO: test for data: string
   it('should send the event { name: helloname, data: { test: "hellodata" } }', function (done) {
     var event = { name: 'helloname', data: { test: 'hellodata' } };
 
@@ -52,6 +54,52 @@ describe('SCXML-CLI - send', function () {
 
     nixt({ colors: false, newlines: false })
       .run(util.client + 'send ' + instanceId + ' ' + event.name + ' \'' + JSON.stringify(event.data) + '\'')
+      .expect(util.checkStderr)
+      .stdout('Current:' + JSON.stringify(resultConf))
+      .end(done);
+  });
+
+  it('should send the event name: helloname, data: @eventData.json', function (done) {
+    var event = { name: 'helloname', data: { test: 'hellodata' } },
+      filePath = util.tempPath + '/eventData.json';
+
+    util.passToTestRunner = function (req, res) {
+      expect(req.path).toBe(util.baseApi + instanceId);
+      expect(req.method).toBe('POST');
+      expect(JSON.parse(req.body)).toEqual(event);
+
+      res.setHeader('X-Configuration', JSON.stringify(resultConf));
+      
+      res.sendStatus(200);
+    };
+
+    fs.writeFileSync(filePath, JSON.stringify(event.data), 'utf-8');
+
+    nixt({ colors: false, newlines: false })
+      .run(util.client + 'send ' + instanceId + ' ' + event.name + ' @' + filePath)
+      .expect(util.checkStderr)
+      .stdout('Current:' + JSON.stringify(resultConf))
+      .end(done);
+  });
+
+  it('should send the event @event.json', function (done) {
+    var event = { name: 'helloname', data: { test: 'hellodata' } },
+      filePath = util.tempPath + '/event.json';
+
+    util.passToTestRunner = function (req, res) {
+      expect(req.path).toBe(util.baseApi + instanceId);
+      expect(req.method).toBe('POST');
+      expect(JSON.parse(req.body)).toEqual(event);
+
+      res.setHeader('X-Configuration', JSON.stringify(resultConf));
+      
+      res.sendStatus(200);
+    };
+
+    fs.writeFileSync(filePath, JSON.stringify(event), 'utf-8');
+
+    nixt({ colors: false, newlines: false })
+      .run(util.client + 'send ' + instanceId + ' @' + filePath)
       .expect(util.checkStderr)
       .stdout('Current:' + JSON.stringify(resultConf))
       .end(done);
